f08db8b12129657e447b9155c2ae25ac8c6c680a
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-04-27 08:06:34 +0530

    Added form validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import styles from "./App.module.css";
 
 export const App = () => {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
 
   const handleChange = (e) => {
     setEmail(e.target.value);
+    error && e.target.value.includes("@") && setError(false);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!email.includes("@")) {
+      setError(true);
+    } else {
+      setEmail("");
+      alert(`Registered your email - ${email}`);
+    }
   };
 
   return (
@@ -28,7 +36,7 @@ export const App = () => {
           <form className={styles.register} onSubmit={handleSubmit}>
             <input
               type="email"
-              className={styles.email}
+              className={`${styles.email} ${error ? styles.error : null}`}
               placeholder="Email Address"
               value={email}
               onChange={handleChange}
@@ -40,6 +48,18 @@ export const App = () => {
                 className={styles["send-image"]}
               />
             </button>
+            {error ? (
+              <img
+                src="/icon-error.svg"
+                alt="Error occured"
+                className={styles["error-image"]}
+              />
+            ) : null}
+            {error ? (
+              <p className={styles["error-text"]}>
+                Please provide a valid email
+              </p>
+            ) : null}
           </form>
         </div>
       </div>
